d5b58865e80d5c6072a2973ebadf5d88857c81c8
Dmytro Hultiaev <82448851+dmytro-hultiaiev@users.noreply.github.com>
2023-06-15 23:17:34 +0300

    IoC and DI practice

---
+++ src/main/java/org/string/practise/IoC/ClassicMusic.java
@@ -0,0 +1,8 @@
+package org.string.practise.IoC;
+
+public class ClassicMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Turkish march";
+    }
+}
---
+++ src/main/java/org/string/practise/IoC/PopMusic.java
@@ -0,0 +1,8 @@
+package org.string.practise.IoC;
+
+public class PopMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Love like a love song";
+    }
+}
